bb1436ba15da8e45abc2717ae6d4c57e3a6cf4bf
Aleksandr <ledich19@gmail.com>
2022-09-11 17:26:31 +0300

    feat: add notes

---
+++ js/dataNotes.js
@@ -0,0 +1,109 @@
+const note = [
+  {
+    "name": "Dianna Rose",
+    "createAt": "2022-06-11T09:38:49 -03:00",
+    "category": "Random Thought",
+    "content": "Consequat pariatur ullamco velit est deserunt reprehenderit sit irure magna.",
+    "date": [
+      "2018-06-09T10:20:01 -03:00",
+      "2020-01-17T05:50:27 -03:00"
+    ],
+    "isArchive": true
+  },
+  {
+    "name": "Catalina Horton",
+    "createAt": "2015-07-27T10:52:10 -03:00",
+    "category": "Random Thought",
+    "content": "Adipisicing adipisicing nisi ut et qui nostrud.",
+    "date": [
+      "2020-08-18T02:32:47 -03:00",
+    ],
+    "isArchive": false
+  },
+  {
+    "name": "Jimenez Solis",
+    "createAt": "2018-09-18T08:44:24 -03:00",
+    "category": "Task",
+    "content": "In Lorem consequat excepteur consequat sint nisi.",
+    "date": [
+      "2019-10-04T10:38:45 -03:00"
+    ],
+    "isArchive": false
+  },
+  {
+    "name": "Cole Davenport",
+    "createAt": "2015-01-12T05:42:23 -03:00",
+    "category": "Idea",
+    "content": "Proident Lorem in nulla nisi sunt veniam eiusmod fugiat enim sit.",
+    "date": [
+      "2017-04-07T03:05:57 -03:00",
+      "2017-12-23T06:00:03 -03:00"
+    ],
+    "isArchive": false
+  },
+  {
+    "name": "Petty House",
+    "createAt": "2018-03-15T03:43:09 -03:00",
+    "category": "Idea",
+    "content": "Lorem veniam tempor consequat dolor proident mollit culpa dolor eiusmod eiusmod magna.",
+    "date": [
+      "2021-05-18T08:10:35 -03:00",
+      "2015-10-22T03:43:06 -03:00",
+      "2018-11-11T12:26:48 -03:00"
+    ],
+    "isArchive": false
+  },
+  {
+    "name": "Winifred Holt",
+    "createAt": "2014-04-10T03:54:01 -04:00",
+    "category": "Idea",
+    "content": "Occaecat quis dolor do culpa.",
+    "date": [
+      "2022-01-27T04:37:53 -03:00",
+      "2022-07-24T10:37:11 -03:00"
+    ],
+    "isArchive": true
+  },
+  {
+    "name": "Holloway Barrett",
+    "createAt": "2022-05-16T03:56:04 -03:00",
+    "category": "Random Thought",
+    "content": "Officia deserunt enim sit id quis laborum ipsum dolor aute irure.",
+    "date": [
+      "2019-12-01T02:35:36 -03:00",
+      "2018-03-11T02:04:29 -03:00"
+    ],
+    "isArchive": false
+  },
+  {
+    "name": "Cervantes Hardin",
+    "createAt": "2014-02-07T10:14:14 -04:00",
+    "category": "Task",
+    "content": "Cillum elit ullamco aliqua ad labore sit qui incididunt est.",
+    "date": [
+      "2022-08-01T03:51:13 -03:00"
+      
+    ],
+    "isArchive": true
+  },
+  {
+    "name": "Vaughn Yates",
+    "createAt": "2018-11-14T11:54:01 -03:00",
+    "category": "Task",
+    "content": "Labore cupidatat tempor est qui culpa nisi aute ad elit.",
+    "date": [
+      "2022-08-10T09:50:52 -03:00"
+    ],
+    "isArchive": true
+  },
+  {
+    "name": "Briana Diaz",
+    "createAt": "2020-11-13T09:41:41 -03:00",
+    "category": "Idea",
+    "content": "Ad nisi in reprehenderit nisi minim enim in do qui mollit ea et ut.",
+    "date": [
+      "2017-02-08T01:10:55 -03:00"
+    ],
+    "isArchive": true
+  }
+]
